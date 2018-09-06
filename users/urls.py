3d8b007f7cacc32b96504bd5d98b163d4d88d86c
Lucas Arthur <lucas.felgueiras@usp.br>
2018-09-06 17:58:04 -0300

    added remain cruds and initiate workgroup app

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,20 @@
 from django.urls import path
-from .views import teachers_list, teachers_new, teachers_update, teachers_delete, students_new_bulk
+from .views import teachers_list, teachers_new, teachers_update, teachers_delete
+from .views import students_new_bulk, students_list, students_new, students_update, students_delete
+from .views import guests_list, guests_new, guests_update, guests_delete
 
 urlpatterns = [
     path('teachers/', teachers_list, name="teachers_list"),
     path('teachers/new/', teachers_new, name="teachers_new"),
     path('teachers/update/<int:id>/', teachers_update, name="teachers_update"),
     path('teachers/delete/<int:id>/', teachers_delete, name="teachers_delete"),
+    path('students/', students_list, name="students_list"),
+    path('students/new/', students_new, name="students_new"),
+    path('students/update/<int:id>/', students_update, name="students_update"),
+    path('students/delete/<int:id>/', students_delete, name="students_delete"),
     path('students/new/bulk/', students_new_bulk, name="students_new_bulk"),
+    path('guests/', guests_list, name="guests_list"),
+    path('guests/new/', guests_new, name="guests_new"),
+    path('guests/update/<int:id>/', guests_update, name="guests_update"),
+    path('guests/delete/<int:id>/', guests_delete, name="guests_delete"),
 ]
--- users/views.py
+++ users/views.py
@@ -1,26 +1,10 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from .models import  User, Student, Teacher, Guest, Coordinator
-from .forms import  TeachersForm, GuestsForm, CoordinatorsForm, StudentBulkRegisterForm
+from .forms import  UsersForm, StudentsForm, TeachersForm, GuestsForm, CoordinatorsForm, StudentBulkRegisterForm
 from .utils import xls_jupiter_parser
 
-# Create your views here.
-
-@login_required
-def students_new_bulk(request):
-    if not request.user.is_superuser:
-        return render(request, 'statuses/401.html')
-
-    student_bulk_register_form = StudentBulkRegisterForm(request.POST or None, request.FILES or None)
-
-    if request.method == 'POST' and student_bulk_register_form.is_valid():
-        new_students_xls = request.FILES['students_xls']
-
-        xls_jupiter_parser(new_students_xls)
-
-        return redirect('disciplines_new')
-    return render(request, 'students/new_bulk_form.html', {'student_bulk_register_form': student_bulk_register_form})
-
+# Teacher views
 
 @login_required
 def teachers_list(request):
@@ -64,7 +48,7 @@ def teachers_update(request, id):
         new_user.username = new_user.email
         new_user.save()
 
-        new_teacher = teachers_form.save()
+        teachers_form.save()
         return redirect('teachers_list')
 
     return render(request, 'teachers/teacher_form.html', {'users_form': users_form, 'teachers_form': teachers_form})
@@ -82,3 +66,142 @@ def teachers_delete(request, id):
         return redirect('teachers_list')
 
     return render(request, 'teachers/teacher_delete_confirm.html', {'teacher': teacher})
+
+# Student views
+
+@login_required
+def students_list(request):
+    students = Student.objects.all()
+    return render(request, 'students/students.html', {'students': students})
+
+@login_required
+def students_new(request):
+    if not request.user.is_superuser:
+        return render(request, 'statuses/401.html')
+
+    users_form = UsersForm(request.POST or None)
+    students_form = StudentsForm(request.POST or None)
+
+    if users_form.is_valid() and students_form.is_valid():
+        new_user = users_form.save(commit=False)
+        new_student = students_form.save(commit=False)
+
+        new_user.username = new_user.email
+        new_user.set_password(new_student.usp_number)
+        new_user.save()
+
+        new_student.user_id = new_user.pk
+        new_student.save()
+
+        return redirect('students_list')
+    return render(request, 'students/student_form.html', {'users_form': users_form, 'students_form': students_form})
+
+@login_required
+def students_update(request, id):
+    if not request.user.is_superuser:
+        return render(request, 'statuses/401.html')
+
+    student = get_object_or_404(Student, pk=id)
+    students_form = StudentsForm(request.POST or None, request.FILES or None, instance=student)
+    users_form = UsersForm(request.POST or None, request.FILES or None, instance=student.user)
+
+    if users_form.is_valid() and students_form.is_valid():
+        new_user = users_form.save(commit=False)
+        new_user.username = new_user.email
+        new_user.save()
+
+        students_form.save()
+        return redirect('students_list')
+
+    return render(request, 'students/student_form.html', {'users_form': users_form, 'students_form': students_form})
+
+@login_required
+def students_delete(request, id):
+    if not request.user.is_superuser:
+        return render(request, 'statuses/401.html')
+
+    student = get_object_or_404(Student, pk=id)
+
+    if request.method == 'POST':
+        student.user.delete()
+        student.delete()
+        return redirect('students_list')
+
+    return render(request, 'students/student_delete_confirm.html', {'student': student})
+
+@login_required
+def students_new_bulk(request):
+    if not request.user.is_superuser:
+        return render(request, 'statuses/401.html')
+
+    student_bulk_register_form = StudentBulkRegisterForm(request.POST or None, request.FILES or None)
+
+    if request.method == 'POST' and student_bulk_register_form.is_valid():
+        new_students_xls = request.FILES['students_xls']
+
+        xls_jupiter_parser(new_students_xls)
+
+        return redirect('disciplines_new')
+    return render(request, 'students/new_bulk_form.html', {'student_bulk_register_form': student_bulk_register_form})
+
+# Guest views
+
+@login_required
+def guests_list(request):
+    guests = Guest.objects.all()
+    return render(request, 'guests/guests.html', {'guests': guests})
+
+@login_required
+def guests_new(request):
+    if not request.user.is_superuser:
+        return render(request, 'statuses/401.html')
+
+    users_form = UsersForm(request.POST or None)
+    guests_form = GuestsForm(request.POST or None)
+
+    if users_form.is_valid() and guests_form.is_valid():
+        new_user = users_form.save(commit=False)
+        new_guest = guests_form.save(commit=False)
+
+        new_user.username = new_user.email
+        new_user.set_password('tccpoliusp')
+        new_user.save()
+
+        new_guest.user_id = new_user.pk
+        new_guest.save()
+
+        return redirect('guests_list')
+    return render(request, 'guests/guest_form.html', {'users_form': users_form, 'guests_form': guests_form})
+
+@login_required
+def guests_update(request, id):
+    if not request.user.is_superuser:
+        return render(request, 'statuses/401.html')
+
+    guest = get_object_or_404(Guest, pk=id)
+    guests_form = GuestsForm(request.POST or None, request.FILES or None, instance=guest)
+    users_form = UsersForm(request.POST or None, request.FILES or None, instance=guest.user)
+
+    if users_form.is_valid() and guests_form.is_valid():
+        new_user = users_form.save(commit=False)
+        new_user.username = new_user.email
+        new_user.save()
+
+        guests_form.save()
+        return redirect('guests_list')
+
+    return render(request, 'guests/guest_form.html', {'users_form': users_form, 'guests_form': guests_form})
+
+@login_required
+def guests_delete(request, id):
+    if not request.user.is_superuser:
+        return render(request, 'statuses/401.html')
+
+    guest = get_object_or_404(Guest, pk=id)
+
+    if request.method == 'POST':
+        guest.user.delete()
+        guest.delete()
+        return redirect('guests_list')
+
+    return render(request, 'guests/guest_delete_confirm.html', {'guest': guest})
