c61895512ad5b9c9e0b13b78852a89ff885fc233
Lucas Arthur <lucas.felgueiras@usp.br>
2018-09-11 16:56:23 -0300

    refactoring deliveries

--- deliveries/urls.py
+++ deliveries/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-from .views import deliveries_list, deliveries_list_by_workgroup, students_deliveries_new
+from .views import deliveries_list, deliveries_new
 
 urlpatterns = [
     path('', deliveries_list, name="deliveries_list"),
-    path('workgroup/<int:workgroup_id>/', deliveries_list_by_workgroup, name="deliveries_list_by_workgroup"),
-    path('workgroup/<int:workgroup_id>/new/', students_deliveries_new, name="students_deliveries_new"),
+    path('new/', deliveries_new, name="deliveries_new"),
 ]
--- deliveries/views.py
+++ deliveries/views.py
@@ -1,27 +1,30 @@
+from django.db.models import Q
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from .models import  Delivery
 from .forms import StudentsDeliveriesForm
 from workgroups.models import Workgroup
+from activities.models import Activity
 
 @login_required
 def deliveries_list(request, activity_id):
-    if not request.user.is_superuser:
-        return render(request, 'statuses/401.html')
+    if request.user.is_superuser:
+        deliveries = Delivery.objects.filter(activity__id=activity_id)
+    else:
+        deliveries = Delivery.objects.filter(Q(workgroup__students__in=[request.user]) | Q(workgroup__advisor=request.user) | Q(workgroup__guest=request.user), activity__id=activity_id)
+
+    activity = get_object_or_404(Activity, pk=activity_id)
 
     deliveries = Delivery.objects.filter(activity__id=activity_id)
-    return render(request, 'deliveries.html', {'deliveries': deliveries, 'activity_id': activity_id})
+    return render(request, 'deliveries.html', {'deliveries': deliveries, 'activity': activity})
 
 @login_required
-def deliveries_list_by_workgroup(request, activity_id, workgroup_id):
-    deliveries = Delivery.objects.filter(activity__id=activity_id, workgroup__id=workgroup_id)
-    return render(request, 'deliveries_list_by_workgroup.html', {'deliveries': deliveries, 'activity_id': activity_id, 'workgroup_id': workgroup_id})
+def deliveries_new(request, activity_id):
+    activity = get_object_or_404(Activity, pk=activity_id)
 
-@login_required
-def students_deliveries_new(request, activity_id, workgroup_id):
-    workgroup = get_object_or_404(Workgroup, pk=workgroup_id)
+    workgroup = Workgroup.objects.filter(students__in=[request.user]).order_by('-created_at').first()
 
-    if not request.user in workgroup.students.all():
+    if not workgroup or activity.is_closed:
         return render(request, 'statuses/401.html')
 
     students_deliveries_form = StudentsDeliveriesForm(request.POST or None, request.FILES or None)
@@ -29,8 +32,8 @@ def students_deliveries_new(request, activity_id, workgroup_id):
     if students_deliveries_form.is_valid():
         new_delivery = students_deliveries_form.save(commit=False)
         new_delivery.activity_id = activity_id
-        new_delivery.workgroup_id = workgroup_id
+        new_delivery.workgroup_id = workgroup.pk
         new_delivery.save()
 
-        return redirect('deliveries_list_by_workgroup', activity_id=activity_id, workgroup_id=workgroup_id)
-    return render(request, 'student_delivery_form.html', {'students_deliveries_form': students_deliveries_form, 'activity_id': activity_id, 'workgroup_id': workgroup_id})
+        return redirect('deliveries_list_workgroup', activity_id=activity_id, workgroup_id=workgroup.pk)
+    return render(request, 'student_delivery_form.html', {'students_deliveries_form': students_deliveries_form, 'activity': activity})
