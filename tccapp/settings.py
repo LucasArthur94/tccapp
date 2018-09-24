00ec657c8dec94262aff41a74de6d66050da742b
Lucas Arthur <lucas.felgueiras@usp.br>
2018-09-24 14:27:32 -0300

    ready to open repo

--- tccapp/settings.py
+++ tccapp/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY', default='+a)f8!76t!^(_w1021=ztt*2h(likl#l)t0g**m(dz_=*ur9n@')
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
@@ -168,7 +168,7 @@ STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-GOOGLE_DRIVE_STORAGE_JSON_KEY_FILE = 'statics/json/gdrive.json'
+GOOGLE_DRIVE_STORAGE_JSON_KEY_FILE = 'staticfiles/json/gdrive.json'
 
 LOGIN_URL = '/login/'
 
