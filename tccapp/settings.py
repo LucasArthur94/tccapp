512cb3370aa7122cf6eca0a43e5f798a8565e258
Lucas Arthur <lucas.felgueiras@usp.br>
2018-09-27 11:51:35 -0300

    update with new domain

--- tccapp/settings.py
+++ tccapp/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['tccapp-next-release.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['tccapp-next-release.herokuapp.com', 'tccapp-staging.herokuapp.com', 'localhost']
 
 RUNNING_DOMAIN = config('RUNNING_DOMAIN', default='localhost')
 
