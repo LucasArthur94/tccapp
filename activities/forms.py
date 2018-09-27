05132472d6c36a0ac350b0e03c0770314ce1cb04
Lucas Arthur <lucas.felgueiras@usp.br>
2018-09-27 12:31:02 -0300

    added validation in dates

--- activities/forms.py
+++ activities/forms.py
@@ -1,20 +1,24 @@
-from django.forms import ModelForm, DateInput
+from django.core.exceptions import ValidationError
+from django.forms import ModelForm, DateField
+from datetime import date
 from .models import  Activity
 from django.utils.translation import gettext_lazy as _
 
 class ActivitiesForm(ModelForm):
+    def validate_retroative_date(value):
+        if value < date.today():
+            raise ValidationError(u'Insira uma data limite válida!')
+
+    due_date = DateField(label='Data Limite de Entrega', validators=[validate_retroative_date])
+
     class Meta:
         model = Activity
         fields = ['name', 'weight', 'due_date', 'main_file_name', 'main_file_required', 'side_file_name', 'side_file_required']
         labels = {
-            'name': ('Nome da Atividade'),
-            'weight': ('Peso da Avaliação'),
-            'due_date': ('Data Limite de Entrega'),
-            'main_file_name': ('Nome do Arquivo Principal'),
-            'main_file_required': ('Arquivo Principal Necessário?'),
-            'side_file_name': ('Nome do Arquivo Extra'),
-            'side_file_required': ('Arquivo Extra Necessário?'),
-        }
-        widgets = {
-            'due_date': DateInput(),
+            'name': _('Nome da Atividade'),
+            'weight': _('Peso da Avaliação'),
+            'main_file_name': _('Nome do Arquivo Principal'),
+            'main_file_required': _('Arquivo Principal Necessário?'),
+            'side_file_name': _('Nome do Arquivo Extra'),
+            'side_file_required': _('Arquivo Extra Necessário?'),
         }
--- disciplines/forms.py
+++ disciplines/forms.py
@@ -1,23 +1,27 @@
-from django.forms import ModelForm, DateInput
-from users.models import User
+from django.core.exceptions import ValidationError
+from django.forms import ModelForm, DateField
+from datetime import date
 from .models import  Discipline
 from users.models import User
 from django.utils.translation import gettext_lazy as _
 from django_select2.forms import ModelSelect2MultipleWidget
 
 class DisciplinesForm(ModelForm):
+    def validate_retroative_date(value):
+        if value < date.today():
+            raise ValidationError(u'Insira uma data de término válida!')
+
+    start_date = DateField(label='Data de Início')
+    end_date = DateField(label='Data de Término', validators=[validate_retroative_date])
+
     class Meta:
         model = Discipline
         fields = ['modality', 'users', 'start_date', 'end_date']
         labels = {
             'modality': _('Modalidade'),
             'users': _('Alunos'),
-            'start_date': _('Data de Início'),
-            'end_date': _('Data de Término'),
         }
         widgets = {
-            'start_date': DateInput(),
-            'end_date': DateInput(),
             'users': ModelSelect2MultipleWidget(
                 queryset=User.objects.filter(student__isnull=False),
                 search_fields=['name__icontains', 'email__icontains']
